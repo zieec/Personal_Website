09e7b7d2851c72c14d1aaff15fe1ef2c92a62a84
zieec <154614734+zieec@users.noreply.github.com>
2024-07-27 22:58:39 +0700

    Update projectss_mobile_desktop.js

--- projectss_mobile_desktop.js
+++ projectss_mobile_desktop.js
@@ -273,7 +273,7 @@ function buttonLinksStyle(txt) {
 // Buat tombol dan terapkan gaya
 const seeLive = document.createElement('a');
 seeLive.innerHTML = projectButtons.button1; // Hanya teks "Home"
-seeLive.href = '/Personal_Website/#section1'; // Href untuk navigasi
+seeLive.href = '/Personal_Website'; // Href untuk navigasi
 buttonLinksStyle(seeLive);
 
 // Kontainer tombol
